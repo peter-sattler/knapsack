10802c1218fcd47cf71331e188ed0b0873260857
Peter Sattler <peter-sattler@users.noreply.github.com>
2018-11-29 15:29:27 -0500

    REFACTOR: Extend from KnapsackBasePackerImpl class

--- src/main/java/com/sattler/knapsack/KnapsackWholeItemRecursivePackerImpl.java
+++ src/main/java/com/sattler/knapsack/KnapsackWholeItemRecursivePackerImpl.java
@@ -11,63 +11,58 @@ import org.slf4j.LoggerFactory;
 import com.sattler.knapsack.Knapsack.Item;
 
 /**
- * Knapsack whole item packer implemented using simple, naive recursion. It does not allow 
+ * Knapsack whole item packer implemented using simple, naive recursion. It does not allow
  * fractional items to be packed. That is, an item is either packed or it is left out.
+ * <p/>
+ * <b>WARNING:</b> This algorithm suffers from quadratic performance due to massive
+ * recomputation of overlapping subproblems
  * 
  * @see <a href="http://techieme.in/solving-01-knapsack-problem-using-recursion">Solving 0/1 Knapsack problem using Recursion</a>
  * @author Pete Sattler
  * @version November 2018
  */
-public final class KnapsackWholeItemRecursivePackerImpl implements KnapsackPacker, Serializable {
+public final class KnapsackWholeItemRecursivePackerImpl extends KnapsackBasePackerImpl implements Serializable {
 
     private static final long serialVersionUID = -4277676325805820861L;
     private static final Logger LOGGER = LoggerFactory.getLogger(KnapsackWholeItemRecursivePackerImpl.class);
-    private final Knapsack knapsack;
 
     /**
-     * Constructs a new knapsack packer
+     * Constructs a new whole item recursive knapsack packer
      * 
      * @param knapsack The knapsack to pack
+     * @param items The items to pack
      */
-    public KnapsackWholeItemRecursivePackerImpl(Knapsack knapsack) {
-        this.knapsack = knapsack;
+    public KnapsackWholeItemRecursivePackerImpl(Knapsack knapsack, Item[] items) {
+        super(knapsack, items);
     }
 
     @Override
-    public Knapsack pack(Item[] items) {
-        packImpl(0, items, knapsack.getCapacity(), 0, 0);
-        return knapsack;
+    public synchronized void pack() {
+        packImpl(0, 0, knapsack.getCapacity(), 0);
     }
 
-    private Item packImpl(int recursionLevel, Item[] items, BigDecimal remainingCapacity, int totalCost, int itemNbr) {
+    private Item packImpl(int recursionLevel, int itemNbr, BigDecimal remainingCapacity, int totalCost) {
         if (remainingCapacity == BigDecimal.ZERO || itemNbr == items.length) {
             LOGGER.debug("RECURSION-LEVEL={}: End of recursion path, remaining capacity={}, total cost={}", recursionLevel, remainingCapacity, totalCost);
-            return ZERO_ITEM;
+            return Item.ZERO;
         }
         final Item currentItem = items[itemNbr];
         LOGGER.debug("RECURSION-LEVEL={}: Considering {}", recursionLevel, currentItem);
         if (currentItem.getWeight().compareTo(remainingCapacity) > 0) {
             LOGGER.debug("RECURSION-LEVEL={}: {} does not fit into the remaining capacity of {} lbs.", recursionLevel, currentItem, remainingCapacity);
-            return packImpl(++recursionLevel, items, remainingCapacity, totalCost, itemNbr + 1);
+            return packImpl(++recursionLevel, itemNbr + 1, remainingCapacity, totalCost);
         }
-        //Let the specific knapsack implementation handle the retention of the most expensive items:
-        final Item rightBranch = currentItem.add(packImpl(++recursionLevel, items, remainingCapacity.subtract(currentItem.getWeight()), totalCost, itemNbr + 1));
-        final Item leftBranch = packImpl(++recursionLevel, items, remainingCapacity, totalCost, itemNbr + 1);
-        //The RIGHT branch is the solution where the parent node is chosen:
-        if (knapsack.add(rightBranch)) {
-            LOGGER.debug("RECURSION-LEVEL={}: {} from RIGHT branch fits into the knapsack", recursionLevel, rightBranch);
-            return rightBranch;
+        //Either take the item or leave it:
+        //NOTE: The knapsack implementation handles retention of the most expensive item(s)
+        final Item leftItem = packImpl(++recursionLevel, itemNbr + 1, remainingCapacity, totalCost);
+        if (knapsack.add(currentItem)) {
+            LOGGER.debug("RECURSION-LEVEL={}: Current {} fits into the knapsack", recursionLevel, currentItem);
+            return currentItem;
         }
-        //The LEFT branch is the solution where the parent node is not chosen:
-        if (knapsack.add(leftBranch)) {
-            LOGGER.debug("RECURSION-LEVEL={}: {} from LEFT branch fits into the knapsack", recursionLevel, leftBranch);
-            return leftBranch;
+        if (knapsack.add(leftItem)) {
+            LOGGER.debug("RECURSION-LEVEL={}: Left {} fits into the knapsack", recursionLevel, leftItem);
+            return leftItem;
         }
-        return ZERO_ITEM;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s [knapsack=%s]", getClass().getSimpleName(), knapsack);
+        return Item.ZERO;
     }
 }
